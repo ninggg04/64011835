d1e2a5dae2581918b8606ebf761e861f9028eba1
ninggg04 <ningming559@gmail.com>
2024-11-22 19:58:28 +0700

    commit1

---
+++ index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App"; // Import คอมโพเนนต์หลักของแอป
+
+// เรียกใช้งาน App และเรนเดอร์ไปยัง HTML
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root") // เชื่อมกับ div ที่มี id="root" ใน index.html
+);
---
+++ me.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Me = () => {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <p>Hello! I'm a React developer passionate about creating awesome web applications.</p>
+    </section>
+  );
+};
+
+export default Me;
